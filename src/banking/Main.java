c24decd19ae4274accc7e769c6869b74b271d878
vikashalikatti <vikaslahalikatti117@gmail.com>
2023-07-25 16:43:30 +0530

    banking

---
+++ src/banking/Main.java
@@ -0,0 +1,12 @@
+package banking;
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	}
+
+}
